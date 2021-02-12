410b39b8a238885f1aabc53ca6d7cdcd38f49efc
carlosbot666777 <78806018+carlosbot666777@users.noreply.github.com>
2021-02-12 17:06:41 -0300

    Add files via upload

---
+++ src/nabutomenu.js
@@ -0,0 +1,12 @@
+const carlosmenu = (prefix, pushname) => {
+    return `◪ *Comandos do ninem*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.carlosmenu = carlosmenu
